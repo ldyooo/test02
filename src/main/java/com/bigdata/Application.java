7b6119453410aa4d577b120b68bc66063f81b4cf
ldyooo <qwer_88506539@sina.com>
2019-12-24 17:33:07 +0800

    idea第一次提交

--- src/main/java/com/bigdata/Application.java
+++ src/main/java/com/bigdata/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+        System.out.println("hhhh");
     }
 }
